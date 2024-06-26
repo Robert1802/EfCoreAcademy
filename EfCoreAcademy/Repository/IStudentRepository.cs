0163dfc2be645d616ce861b5b5696e986e162a38
Roberto Moura <roblm_@hotmail.com>
2024-06-26 20:17:56 -0300

    Add project files.

---
+++ EfCoreAcademy/Repository/IStudentRepository.cs
@@ -0,0 +1,12 @@
+﻿using EfCoreAcademy.Model;
+
+namespace EfCoreAcademy.Repository;
+
+public interface IStudentRepository
+{
+    Task<int> CreateStudentAsync(Student student);
+    Task UpdateStudentAsync(Student student);
+    Task<Student?> GetStudentByIdAsync(int studentId);
+    Task<List<Student>> GetAllStudentsAsync();
+    Task DeleteStudentAsync(int studentId);
+}
